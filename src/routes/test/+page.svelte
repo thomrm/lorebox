6387d7013e196a1f8ec602625f7efda5f6867bf9
Thomas Reed-Munoz <thomas@Thoms-MacBook-Pro.local>
2024-05-10 08:50:42 -0400

    lorcast api test

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    onMount(async () => {
+        // Test fetch
+        const response = await fetch('https://api.lorcast.com/v0');
+        const data = await response.json();
+
+        console.log(data);
+    });
+
+</script>
+
+
+<div>
+    This is a test page.
+</div>
+
+<style>
+</style>
